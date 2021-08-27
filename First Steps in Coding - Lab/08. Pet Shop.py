9e0d0bbd1ed36c1ddcea2b8e465dfcd5cea8d3e8
Silvia <80534941+SilviaKoynova@users.noreply.github.com>
2021-08-27 10:19:48 +0300

    Add files via upload

---
+++ First Steps in Coding - Lab/08. Pet Shop.py
@@ -0,0 +1,6 @@
+num_dogs = int(input())
+num_other = int(input())
+sum_dogs = num_dogs * 2.50
+sum_other = num_other * 4
+result = sum_dogs + sum_other
+print(f"{result} lv")
---
+++ First Steps in Coding - Lab/09. Yard Greening.py
@@ -0,0 +1,6 @@
+cm2 = float(input())
+whole_garden_price = cm2 * 7.61
+discount_price = 0.18 * whole_garden_price
+final_price = whole_garden_price - discount_price
+print(f"The final price is: {final_price:.2f} lv.")
+print(f"The discount price is: {discount_price:.2f} lv.")
